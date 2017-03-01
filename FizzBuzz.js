43606c9a82c6d60a8648a2a5a7cd36836f22ba12
Steven Strasburg <Strasburg@Stevens-MacBook-Air.local>
2017-02-28 16:12:44 -0800

    JavaScript number iteriation and divisible checker using FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,19 @@
+
+
+function fizzBuzz() {
+
+  var countTo = input;
+  for(var i=1;i<=countTo;i++){
+    		output = '';
+
+        if( i%3 === 0 ) {
+          output += Fizz
+        }
+
+        if( i%5 === 0 ) {
+          output += Buzz
+        }
+
+        print(output);
+  }
+}
